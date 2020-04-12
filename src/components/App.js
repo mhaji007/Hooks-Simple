466aeb00737fa767282f0d24219e6e056d3e3534
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-12 15:09:33 -0400

    Refactor with useState component

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+
+const App =  () => {
+    
+        const [resource, setResource] = useState('posts');
+        
+        return (
+        <div>
+            <div>
+                <button onClick={()=>setResource('post')}>Posts</button>
+                <button onClick={()=>setResource('todos')}>ToDos</button>
+            </div>
+            {resource}
+        </div>);
+    
+}
+
+export default App;
