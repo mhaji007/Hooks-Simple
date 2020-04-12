8892d85a5cbff2d3fe525072c2298a204da9135b
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-12 17:40:16 -0400

    Add componentDidMount and omponentDidUpdate

--- src/components/ResourceList.jsx
+++ src/components/ResourceList.jsx
@@ -7,10 +7,25 @@ class ResourceList extends React.Component {
     state = {resources: []};
 
     async componentDidMount() {
-        const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
         
         this.setState({resources:response.data});
     }
+
+    async componentDidUpdate (prevProps) {
+        if(prevProps.resource !== this.props.resource) {
+            const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+        
+        this.setState({resources:response.data});
+        }
+    }
+
+    // async componentDidUpdate (){
+        // const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
+        
+        // this.setState({resources:response.data});
+        
+    // }
 
     render() { 
         return (
